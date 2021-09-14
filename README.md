008f2404b160fcf4773555c06c9fb1ae6e25c2b7
Vladimir Sadovnikov <sadovnikov.vs@yandex.ru>
2021-09-14 05:49:12 +0300

    homework_01

---
+++ README.md
@@ -0,0 +1,5 @@
+# Homeworks Python Base Course
+
+## Выполненые домашние работы по курсу
+- homework_01
+
---
+++ homework_01/main.py
@@ -0,0 +1,64 @@
+"""
+Домашнее задание №1
+Функции и структуры данных
+"""
+
+
+def power_numbers(*nums):
+    """
+    функция, которая принимает N целых чисел,
+    и возвращает список квадратов этих чисел
+    >>> power_numbers(1, 2, 5, 7)
+    <<< [1, 4, 25, 49]
+    """
+    squares_nums = []
+    for num in nums:
+        if isinstance(num, int) and num >= 0:
+            squares_nums.append(num ** 2)
+    return squares_nums
+
+
+# filter types
+ODD = "odd"
+EVEN = "even"
+PRIME = "prime"
+
+
+def is_prime(num):
+    """
+    Функция, на вход принимает число,
+    проверяет простое число или нет, и 
+    возвращает True - простое,
+    False - составное.
+
+    >>> is_prime(5)
+    <<< True
+    >>> is_prime(4)
+    <<< False
+    """
+    d = 2
+    while d * d <= num and num % d != 0:
+        d += 1
+    if d * d > num and num != 1:
+        return True
+    else:
+        return False
+
+
+def filter_numbers(nums, num_type):
+    """
+    функция, которая на вход принимает список из целых чисел,
+    и возвращает только чётные/нечётные/простые числа
+    (выбор производится передачей дополнительного аргумента)
+
+    >>> filter_numbers([1, 2, 3], ODD)
+    <<< [1, 3]
+    >>> filter_numbers([2, 3, 4, 5], EVEN)
+    <<< [2, 4]
+    """
+    results = {ODD: [], EVEN: [], PRIME: []}
+    if isinstance(nums,(tuple, list)) and num_type in results.keys():
+        results[EVEN] = list(filter(lambda num: (num % 2) == 0, nums))
+        results[ODD] = list(filter(lambda num: (num % 2) == 1, nums))
+        results[PRIME] = list(filter(lambda num: is_prime(num), nums))
+        return results[num_type]
